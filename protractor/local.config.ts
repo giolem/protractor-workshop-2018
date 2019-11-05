4e2990bb951c7a841ec8fc1b484b67d1cc90ddad
Johany Andres Quintero Toro <jquinterot@psl.com.co>
2019-11-05 08:38:04 -0500

    Adding implicit times using Jasmin

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,11 +4,16 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 30000 ,
+
   SELENIUM_PROMISE_MANAGER : false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
   }
 
 };
--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -13,10 +13,6 @@ describe('Buy a t-shirt', () => {
   const productAddedModalPage : ProductAddedModalPage = new ProductAddedModalPage();
   const orderSummaryPage : OrderSummaryPage = new OrderSummaryPage();
 
-  beforeEach(() => {
-
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-  });
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await(browser.sleep(3000));
